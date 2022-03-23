8ab6f793ce345a88cc288798dbe0a110529014f1
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-03-23 17:16:49 +0600

    render  thread and post

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,19 @@
 module.exports = {
-    root: true,
+  root: true,
 
-    env: {
-        node: true
-    },
+  env: {
+    node: true,
+  },
 
-    extends: [
-        'plugin:vue/vue3-essential',
-        '@vue/standard'
-    ],
+  extends: ["plugin:vue/vue-social", "@vue/standard"],
 
-    parserOptions: {
-        parser: 'babel-eslint'
-    },
+  parserOptions: {
+    parser: "babel-eslint",
+  },
 
-    rules: {
-        'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-    }
-}
+  rules: {
+    "no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "warn",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
