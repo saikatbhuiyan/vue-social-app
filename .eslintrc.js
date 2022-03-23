5e6bbc16603f0ee5855353ecb023548438cc74dc
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-03-23 19:21:18 +0600

    fix bug and add style

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,18 @@ module.exports = {
   root: true,
 
   env: {
-    node: true,
+    node: true
   },
 
-  extends: ["plugin:vue/vue-social", "@vue/standard"],
+  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
 
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint'
   },
 
   rules: {
-    "no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "warn",
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    'no-unused-vars': 'warn',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <page-home/>
+  <div class="container">
+    <page-home />
+  </div>
 </template>
 
 <script>
