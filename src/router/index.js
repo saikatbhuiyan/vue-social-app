2f24eb4ec7f5923de3c13791cb6eb62cbf98d66a
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-04-09 02:26:58 +0600

    redirect to Not Found page without changing the URL

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import PageHome from '@/components/PageHome'
 import PageThreadShow from '@/components/PageThreadShow'
 import PageNotFound from '@/components/PageNotFound'
+import sourceData from '@/data.json'
 
 import { createRouter, createWebHistory } from 'vue-router'
 
@@ -14,7 +15,24 @@ const routes = [
     path: '/thread/:id',
     name: 'ThreadShow',
     component: PageThreadShow,
-    props: true
+    props: true,
+    beforeEnter (to, from, next) {
+      // check if thread exists
+      const threadExists = sourceData.threads.find(thread => thread.id === to.params.id)
+      // if exists continue
+      if (threadExists) {
+        return next()
+      } else {
+        next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          // preserve existing query and hash
+          query: to.query,
+          hash: to.hash
+        })
+      }
+      // if doesnt exist redirect to not found
+    }
   },
   {
     path: '/:pathMatch(.*)*',
