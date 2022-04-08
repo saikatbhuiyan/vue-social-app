e81832ff66f667df4491ab40701c06778905ccc8
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-04-09 00:12:58 +0600

    PageThreadShow component

--- src/main.js
+++ src/main.js
@@ -3,12 +3,19 @@ import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 import PageHome from '@/components/PageHome'
+import PageThreadShow from '@/components/PageThreadShow'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: PageHome
+  },
+  {
+    path: '/thread/:id',
+    name: 'ThreadShow',
+    component: PageThreadShow,
+    props: true
   }
 ]
 const router = createRouter({
