338b8836f532defb1da3b64c92ac9cf8bced5bc2
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-04-09 00:31:19 +0600

    refactor router configuration

--- src/main.js
+++ src/main.js
@@ -1,27 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import router from '@/router'
 
-import PageHome from '@/components/PageHome'
-import PageThreadShow from '@/components/PageThreadShow'
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: PageHome
-  },
-  {
-    path: '/thread/:id',
-    name: 'ThreadShow',
-    component: PageThreadShow,
-    props: true
-  }
-]
-const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
 const forumApp = createApp(App)
 
 forumApp.use(router)
