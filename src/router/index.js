90b0cb8977e2403f554746094715f951a03e34f8
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-05-29 05:27:28 +0600

    creating the user profile

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import ThreadShow from '@/pages/ThreadShow'
 import NotFound from '@/pages/NotFound'
 import Forum from '@/pages/Forum'
 import Category from '@/pages/Category'
+import Profile from '@/pages/Profile'
 import sourceData from '@/data.json'
 
 import { createRouter, createWebHistory } from 'vue-router'
@@ -12,6 +13,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/me',
+    name: 'Profile',
+    component: Profile
   },
   {
     path: '/category/:id',
